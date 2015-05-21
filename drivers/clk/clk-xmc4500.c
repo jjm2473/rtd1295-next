c808dfdb30bc026fa7058f0ff4ce162ccde95561
Andreas Färber <afaerber@suse.de>
2015-05-21 07:47:29 +0200

    clk: Add CCU gate clock
    Signed-off-by: Andreas Färber <afaerber@suse.de>

--- drivers/clk/clk-xmc4500.c
+++ drivers/clk/clk-xmc4500.c
@@ -6,6 +6,79 @@
 #include <linux/io.h>
 #include <linux/of.h>
 #include <linux/of_address.h>
+#include <linux/slab.h>
+
+struct xmc4500_clk {
+	struct clk_hw hw;
+	void __iomem *base;
+	u8 bit_idx;
+};
+
+#define to_xmc_clk(_hw) container_of(_hw, struct xmc4500_clk, hw)
+
+static int xmc4500_clk_is_enabled(struct clk_hw *hw)
+{
+	struct xmc4500_clk *xmcclk = to_xmc_clk(hw);
+	u32 reg;
+
+	reg = clk_readl(xmcclk->base);
+
+	return reg & BIT(xmcclk->bit_idx) ? 1 : 0;
+}
+
+static int xmc4500_clk_enable(struct clk_hw *hw)
+{
+	struct xmc4500_clk *xmcclk = to_xmc_clk(hw);
+
+	clk_writel(BIT(xmcclk->bit_idx), xmcclk->base + 0x4);
+
+	return 0;
+}
+
+static void xmc4500_clk_disable(struct clk_hw *hw)
+{
+	struct xmc4500_clk *xmcclk = to_xmc_clk(hw);
+
+	clk_writel(BIT(xmcclk->bit_idx), xmcclk->base + 0x8);
+}
+
+static const struct clk_ops xmc4500_clk_ops = {
+	.enable		= xmc4500_clk_enable,
+	.disable	= xmc4500_clk_disable,
+	.is_enabled	= xmc4500_clk_is_enabled,
+};
+
+static struct clk *xmc4500_register_gate_clk(const char *name, const char *parent_name,
+	void __iomem *reg_base, u8 bit_idx)
+{
+	struct xmc4500_clk *xmcclk;
+	struct clk_init_data init;
+	struct clk *clk;
+
+	xmcclk = kzalloc(sizeof(*xmcclk), GFP_KERNEL);
+	if (!xmcclk) {
+		return ERR_PTR(-ENOMEM);
+	}
+
+	init.name = name;
+	init.ops = &xmc4500_clk_ops;
+	init.flags = 0;
+	init.parent_names = parent_name ? &parent_name : NULL;
+	init.num_parents = parent_name ? 1 : 0;
+
+	xmcclk->base = reg_base;
+	xmcclk->bit_idx = bit_idx;
+	xmcclk->hw.init = &init;
+
+	clk = clk_register(NULL, &xmcclk->hw);
+	if (IS_ERR(clk)) {
+		pr_err("could not register clk %s\n", name);
+		kfree(xmcclk);
+		return clk;
+	}
+
+	return clk;
+}
 
 enum {
 	CLK_CPU = 0,
@@ -54,8 +127,9 @@ static __init void xmc4500_scu_ccu_init(struct device_node *node)
 		base + 0x10, 0, 1, 0, NULL);
 	clk[CLK_PERIPH] = clk_register_divider(NULL, "PERIPH", "CPU", 0,
 		base + 0x14, 0, 1, 0, NULL);
-	clk[CLK_CCU] = clk_register_divider(NULL, "CCU", "SYS", 0,
+	clk_register_divider(NULL, "CCU", "SYS", 0,
 		base + 0x20, 0, 1, 0, NULL);
+	clk[CLK_CCU] = xmc4500_register_gate_clk("ccu", "CCU", base, 4);
 
 	clk[CLK_EBU] = clk_register_divider(NULL, "EBU", "PLL", 0,
 		base + 0x1C, 0, 6, 0, NULL);
