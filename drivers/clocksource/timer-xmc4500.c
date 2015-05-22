dc47b4697ff86137d7467f06b551d3a269698c1f
Andreas Färber <afaerber@suse.de>
2015-05-22 04:48:57 +0200

    clocksource changes
    Signed-off-by: Andreas Färber <afaerber@suse.de>

--- drivers/clocksource/timer-xmc4500.c
+++ drivers/clocksource/timer-xmc4500.c
@@ -23,18 +23,29 @@ struct xmc4500_clock_event_device {
 
 #define CCU4_GCTRL	0x000
 
+#define CCU4_GSTAT	0x004
+
 #define CCU4_GIDLC	0x00C
 #define CCU4_GIDLC_SPRB		BIT(8)
+
+#define CCU4_GCSS	0x010
+#define CCU4_GCSS_S0SE		BIT(0)
+
+#define CCU4_GCST	0x018
+
+#define CCU4_CC40TST	0x08
 
 #define CCU4_CC40TCSET	0x0C
 #define CCU4_CC4yTCSET_TRBS	BIT(0)
 
 #define CCU4_CC40TCCLR	0x10
 #define CCU4_CC4yTCCLR_TRBC	BIT(0)
+#define CCU4_CC4yTCCLR_TCC	BIT(1)
 
 #define CCU4_CC40TC	0x14
 #define CCU4_CC4yTC_TCM		BIT(0)
 #define CCU4_CC4yTC_TSSM	BIT(1)
+#define CCU4_CC4yTC_CLST	BIT(2)
 #define CCU4_CC4yTC_ENDM	(2UL << 8)
 #define CCU4_CC4yTC_STRM	BIT(10)
 
@@ -43,6 +54,8 @@ struct xmc4500_clock_event_device {
 #define CCU4_CC40PRS	0x34
 
 #define CCU4_CC40TIMER	0x70
+
+#define CCU4_CC40INTS	0xA0
 
 #define CCU4_CC40INTE	0xA4
 #define CCU4_CC4yINTE_PME	BIT(0)
@@ -65,10 +78,13 @@ static void xmc4500_clock_event_set_mode(enum clock_event_mode mode,
 	u32 tc;
 
 	tc = readl_relaxed(slice_base + CCU4_CC40TC);
+	tc |= CCU4_CC4yTC_CLST;
 	switch (mode) {
 	case CLOCK_EVT_MODE_PERIODIC:
+		writel_relaxed(xmcdev->periodic_reload, slice_base + CCU4_CC40PRS);
 		writel_relaxed(tc & ~CCU4_CC4yTC_TSSM, slice_base + CCU4_CC40TC);
-		//writel_relaxed(xmcdev->periodic_reload, slice_base + CCU4_CC40PRS);
+		writel_relaxed(CCU4_GCSS_S0SE, xmcdev->regs + CCU4_GCSS);
+		writel_relaxed(CCU4_CC4yTCCLR_TRBC | CCU4_CC4yTCCLR_TCC, slice_base + CCU4_CC40TCCLR);
 		writel_relaxed(CCU4_CC4yTCSET_TRBS, slice_base + CCU4_CC40TCSET);
 		break;
 	case CLOCK_EVT_MODE_ONESHOT:
@@ -85,13 +101,22 @@ static irqreturn_t xmc4500_clock_event_handler(int irq, void *opaque)
 	struct xmc4500_clock_event_device *xmcdev = opaque;
 	int slice_offset = xmcdev->slice * 0x100;
 	void __iomem *slice_base = xmcdev->regs + 0x100 + slice_offset;
+	/*u32 gstat, gcst, tst, tc, ints, timer;
 
-	writel_relaxed(CCU4_CC4ySWR_RPM, slice_base + CCU4_CC40SWR);
-	writel_relaxed(CCU4_CC4yTCSET_TRBS, slice_base + CCU4_CC40TCSET);
+	gstat = readl_relaxed(xmcdev->regs + CCU4_GSTAT);
+	gcst = readl_relaxed(xmcdev->regs + CCU4_GCST);
+	tst = readl_relaxed(slice_base + CCU4_CC40TST);
+	tc = readl_relaxed(slice_base + CCU4_CC40TC);
+	ints = readl_relaxed(slice_base + CCU4_CC40INTS);
+	timer = readl_relaxed(slice_base + CCU4_CC40TIMER);*/
+
+	writel_relaxed(CCU4_CC4ySWR_ROM | CCU4_CC4ySWR_RPM, slice_base + CCU4_CC40SWR);
+	//writel_relaxed(CCU4_CC4yTCSET_TRBS, slice_base + CCU4_CC40TCSET);
 
 	xmcdev->evtdev.event_handler(&xmcdev->evtdev);
 
-	pr_info("#");
+	/*pr_debug("CCU4 irq gstat %08x gcst %08x tst %08x tc %08x ints %08x timer %08x\n",
+		gstat, gcst, tst, tc, ints, timer);*/
 
 	return IRQ_HANDLED;
 }
@@ -157,8 +182,8 @@ static __init void xmc4500_ccu4_init(struct device_node *node)
 	writel_relaxed(0, base + CCU4_GCTRL);
 
 	writel_relaxed(0xa /* 1024 */, slice_base + CCU4_CC40PSC);
-	writel_relaxed(DIV_ROUND_CLOSEST(rate, 1024 * HZ), slice_base + CCU4_CC40PRS);
-	writel_relaxed(CCU4_CC4yINTE_PME, slice_base + CCU4_CC40INTE);
+	//writel_relaxed(DIV_ROUND_CLOSEST(rate, 1024 * HZ), slice_base + CCU4_CC40PRS);
+	writel_relaxed(CCU4_CC4yINTE_OME | CCU4_CC4yINTE_PME, slice_base + CCU4_CC40INTE);
 
 	writel_relaxed(BIT(xmc4500_clock_event_device.slice), base + CCU4_GIDLC);
 
