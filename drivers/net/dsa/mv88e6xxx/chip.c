16a30a05b3c5a6a50f5105d9ee5ab6844c10c384
Andreas Färber <afaerber@suse.de>
2016-11-08 05:53:19 +0100

    mv88e6xxx: mv88e6xxx_g1_irq_free(): Free irq
    Signed-off-by: Andreas Färber <afaerber@suse.de>

--- drivers/net/dsa/mv88e6xxx/chip.c
+++ drivers/net/dsa/mv88e6xxx/chip.c
@@ -429,6 +429,8 @@ static void mv88e6xxx_g1_irq_free(struct mv88e6xxx_chip *chip)
 {
 	int irq, virq;
 
+	free_irq(chip->irq, chip);
+
 	for (irq = 0; irq < chip->g1_irq.nirqs; irq++) {
 		virq = irq_find_mapping(chip->g1_irq.domain, irq);
 		irq_dispose_mapping(virq);
