a643c91fdf04139ced2a75c03f801fe2821b12f1
Andreas Färber <afaerber@suse.de>
2017-11-19 18:49:24 +0100

    gpio: Prepare S700
    Signed-off-by: Andreas Färber <afaerber@suse.de>

--- drivers/gpio/gpio-owl.c
+++ drivers/gpio/gpio-owl.c
@@ -140,6 +140,7 @@ static int owl_gpio_get(struct gpio_chip *chip, unsigned int offset)
 
 static const struct of_device_id owl_gpio_of_matches[] = {
 	{ .compatible = "actions,s500-gpio" },
+	{ .compatible = "actions,s700-gpio" },
 	{ }
 };
 
