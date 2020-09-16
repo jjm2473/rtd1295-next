tes-rep/linux

soc: realtek: chip: Detect RTD1293
Logic self-determined by comparing DS418j and DS418 registers.

Signed-off-by: Andreas Färber <afaerber@suse.de>

drivers/soc/realtek/chip.c
@@ -59,6 +59,8 @@ static const char *rtd1295_name(struct device *dev, const struct rtd_soc *s)
 		u32 chipinfo1 = readl_relaxed(base);
 		iounmap(base);
 		if (chipinfo1 & BIT(11)) {
+			if (chipinfo1 & BIT(4))
+				return "RTD1293";
 			return "RTD1296";
 		}
 	}

jjm2473/rtd1295-next

drivers/soc/realtek/chip.c
@@ -75,8 +75,11 @@ static const char *rtd1295_name(struct device *dev, const struct dhc_soc *s)
 		ret = regmap_read(regmap, REG_ISO_CHIP_INFO1, &val);
 		if (ret)
 			dev_warn(dev, "Could not read chip_info1 (%d)\n", ret);
-		else if (val & BIT(11))
+		else if (val & BIT(11)) {
+			if (val & BIT(4))
+				return "RTD1293";
 			return "RTD1296";
+		}
 	}
 
 	return "RTD1295";